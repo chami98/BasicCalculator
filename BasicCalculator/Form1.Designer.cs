4ec3187da87cc4f1395e4601c490289900b9eaaa
chamikara <chamikara98@gmail.com>
2021-01-13 19:38:24 +0530

    Added CE button

--- BasicCalculator/Form1.Designer.cs
+++ BasicCalculator/Form1.Designer.cs
@@ -31,7 +31,9 @@ namespace BasicCalculator
         {
             this.UserInput = new System.Windows.Forms.TextBox();
             this.CalculationResultBox = new System.Windows.Forms.Label();
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.ButtonsPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.CEButton = new System.Windows.Forms.Button();
+            this.ButtonsPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // UserInput
@@ -41,50 +43,62 @@ namespace BasicCalculator
             | System.Windows.Forms.AnchorStyles.Right)));
             this.UserInput.Location = new System.Drawing.Point(12, 39);
             this.UserInput.Name = "UserInput";
-            this.UserInput.Size = new System.Drawing.Size(371, 22);
+            this.UserInput.Size = new System.Drawing.Size(300, 22);
             this.UserInput.TabIndex = 0;
             this.UserInput.TextChanged += new System.EventHandler(this.UserInput_TextChanged);
             // 
             // CalculationResultBox
             // 
-            this.CalculationResultBox.Location = new System.Drawing.Point(12, 64);
+            this.CalculationResultBox.Location = new System.Drawing.Point(9, 76);
             this.CalculationResultBox.Name = "CalculationResultBox";
             this.CalculationResultBox.Size = new System.Drawing.Size(396, 23);
             this.CalculationResultBox.TabIndex = 1;
             this.CalculationResultBox.Text = "Please enter an equation and press enter or =";
             // 
-            // tableLayoutPanel1
+            // ButtonsPanel
             // 
-            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            this.ButtonsPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.tableLayoutPanel1.ColumnCount = 4;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(15, 102);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 5;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(368, 317);
-            this.tableLayoutPanel1.TabIndex = 2;
-            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            this.ButtonsPanel.ColumnCount = 4;
+            this.ButtonsPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.ButtonsPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.ButtonsPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.ButtonsPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.ButtonsPanel.Controls.Add(this.CEButton, 0, 0);
+            this.ButtonsPanel.Location = new System.Drawing.Point(15, 102);
+            this.ButtonsPanel.Name = "ButtonsPanel";
+            this.ButtonsPanel.RowCount = 5;
+            this.ButtonsPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.ButtonsPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.ButtonsPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.ButtonsPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.ButtonsPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.ButtonsPanel.Size = new System.Drawing.Size(297, 209);
+            this.ButtonsPanel.TabIndex = 2;
+            this.ButtonsPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // CEButton
+            // 
+            this.CEButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.CEButton.Location = new System.Drawing.Point(3, 3);
+            this.CEButton.Name = "CEButton";
+            this.CEButton.Size = new System.Drawing.Size(68, 35);
+            this.CEButton.TabIndex = 0;
+            this.CEButton.Text = "CE";
+            this.CEButton.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(395, 431);
-            this.Controls.Add(this.tableLayoutPanel1);
+            this.ClientSize = new System.Drawing.Size(324, 323);
+            this.Controls.Add(this.ButtonsPanel);
             this.Controls.Add(this.CalculationResultBox);
             this.Controls.Add(this.UserInput);
             this.Name = "Form1";
             this.Text = "Basic Calculator";
+            this.ButtonsPanel.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -94,7 +108,8 @@ namespace BasicCalculator
 
         private System.Windows.Forms.TextBox UserInput;
         private System.Windows.Forms.Label CalculationResultBox;
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel ButtonsPanel;
+        private System.Windows.Forms.Button CEButton;
     }
 }
 

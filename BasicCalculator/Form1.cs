1423499e268730a05e577d9d0e5ead5062fa5790
chamikara <chamikara98@gmail.com>
2021-01-14 17:13:05 +0530

    Added method to the delete button

--- BasicCalculator/Form1.cs
+++ BasicCalculator/Form1.cs
@@ -29,96 +29,120 @@ namespace BasicCalculator
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            InsertTextValue("7");
         }
 
         private void button1_Click_1(object sender, EventArgs e)
         {
-
+            InsertTextValue("9");
         }
 
-        #region
+        #region clearing methods
         private void CEButton_Click(object sender, EventArgs e)
         {
             this.UserInput.Text = String.Empty;
+            FocusInputText();
         }
 
         private void DelButton_Click(object sender, EventArgs e)
         {
-
+            if (this.UserInput.Text.Length < this.UserInput.SelectionStart + 1)
+                return;
+                this.UserInput.Text = this.UserInput.Text.Remove(this.UserInput.SelectionStart, 1);
+            
+            
         }
-        #endregion clearing methods
+        #endregion 
         #region number methods
 
         private void button8_Click(object sender, EventArgs e)
         {
-
+            InsertTextValue("8");
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-
+            InsertTextValue("4");
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
-
+            InsertTextValue("5");
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
-
+            InsertTextValue("6");
         }
 
         private void button1_Click_2(object sender, EventArgs e)
         {
-
+            InsertTextValue("1");
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-
+            InsertTextValue("2");
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-
+            InsertTextValue("3");
         }
 
         private void button0_Click(object sender, EventArgs e)
         {
-
+            InsertTextValue("0");
         }
 
         private void dotButton_Click(object sender, EventArgs e)
         {
-
+            InsertTextValue(".");
         }
 
         private void equalButton_Click(object sender, EventArgs e)
         {
-
+            CalculateEquation();
         }
+
         #endregion
+
+        private void CalculateEquation()
+        {
+            
+        }
+
         #region Operator Functions
         private void PercentageButton_Click(object sender, EventArgs e)
         {
-
+            InsertTextValue("%");
         }
 
         private void addButton_Click(object sender, EventArgs e)
         {
-
+            InsertTextValue("+");
         }
 
         private void subtractButton_Click(object sender, EventArgs e)
         {
-
+            InsertTextValue("-");
         }
 
         private void MultiplyButton_Click(object sender, EventArgs e)
         {
+            InsertTextValue("X");
+        }
+        #endregion
 
+        #region Private helpers
+        private void FocusInputText()
+        {
+            this.UserInput.Focus();
+        }
+
+        private void InsertTextValue(string value)
+        {
+            this.UserInput.Text = this.UserInput.Text.Insert(this.UserInput.SelectionStart, value);
         }
         #endregion
     }

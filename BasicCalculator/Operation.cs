092cba4a3754382f680411d2045552c09db568f8
chamikara <chamikara98@gmail.com>
2021-01-17 13:02:29 +0530

    Created public properties in Operation.cs

--- BasicCalculator/Operation.cs
+++ BasicCalculator/Operation.cs
@@ -5,8 +5,38 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace BasicCalculator
+
 {
-    class Operation
+    /// <summary>
+    /// Holds information about a single calculator opeartion
+    /// </summary>
+    public class Operation
     {
+
+        #region public properties
+
+        /// <summary>
+        /// The left side of the operation
+        /// </summary>
+
+        public string LeftSide { get; set; }
+
+        // the right side of the operation
+
+
+        public string RightSide { get; set; }
+ 
+    public OperationType OperationType { get; set; }
+
+        // the inner operation  to be performed initially before this opertaion 
+
+        public Operation InnerOperation { get; set; }
+      
+        #endregion
+
+        public Operation()
+        {
+
+        }
     }
 }
